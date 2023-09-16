b3b200707cf8d72e7f5a4cf399481f13e9da9cc1
Naphat <priasdevo@gmail.com>
2023-09-16 10:55:26 +0700

    feat: preminary done with receiving socket

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,62 +1,68 @@
 import express, { json } from 'express'
-import expressWs from 'express-ws'
 import http from 'http'
-import WebSocket from 'ws'
+import cors from 'cors'
+import { Server, Socket } from 'socket.io'
+const SocketIO = require('socket.io')
 
 import { config } from 'dotenv'
 
 const app = express()
-const wsInstance = expressWs(app)
-const server = http.createServer(app)
-const wss = new WebSocket.Server({ server })
 
-app.use(json())
+app.use(express.json())
+app.use(cors())
+
+const server = http.createServer(app)
+
+// const io: Server = new Server(server, {
+//   cors: {
+//     origin: '*', // Allow requests from your client's origin
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allow all HTTP methods
+//   },
+// })
+const io: Server = SocketIO(server, {
+  cors: {
+    origin: '*', // Allow requests from your client's origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allow all HTTP methods
+    allowedHeaders: '*',
+    credentials: false,
+  },
+})
+
+//const io = socketIo(server)
 
 const clients = new Set<WebSocket>() // Store connected clients
 
-wss.on('connection', (ws) => {
-  // Handle WebSocket connections here for video streaming
-  console.log('WebSocket connected')
-  clients.add(ws)
-  // Simulated video stream
-  const videoStreamInterval = setInterval(() => {
-    // const fakeVideoData = generateFakeVideoFrame() // Implement your video source logic here
-    // ws.send(fakeVideoData) // Send video data to the connected client
-  }, 1000 / 30) // 30 frames per second
+io.on('connection', (socket: Socket) => {
+  console.log('Client connected')
 
-  ws.on('message', (data: string) => {
-    // Broadcast the received data to all connected clients except the sender
-    const message = data.toString()
-
-    console.log('Message receive : ', message)
-    broadcast(data, ws)
+  socket.on('stream', (stream) => {
+    console.log('Receive Stream')
+    console.log(stream)
   })
 
-  ws.on('close', () => {
-    console.log('WebSocket disconnected')
-    clients.delete(ws)
-    clearInterval(videoStreamInterval) // Stop sending video data when the client disconnects
+  socket.on('disconnect', () => {
+    console.log('Client disconnected')
   })
 })
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 server.listen(PORT, () => {
   console.log('Server running in', process.env.NOED_ENV, ' mode on port ', PORT)
 })
 
 // Handle promise rejection
-process.on('unhandledRejection', (err: Error, Promise) => {
-  console.log(`Error: ${err.message}`)
-  // close server
-  server.close(() => process.exit(1))
-})
+// process.on('unhandledRejection', (err: Error, Promise) => {
+//   console.log(`Error: ${err.message}`)
+//   // close server
+//   server.close(() => process.exit(1))
+// })
 
-// Function to broadcast data to all connected clients
-function broadcast(data: string, sender: WebSocket) {
-  clients.forEach((client: WebSocket) => {
-    // Send the data to all clients except the sender
-    if (client !== sender && client.readyState === WebSocket.OPEN) {
-      client.send(data)
-    }
-  })
-}
+// // Function to broadcast data to all connected clients
+// function broadcast(data: string, sender: WebSocket) {
+//   clients.forEach((client: WebSocket) => {
+//     // Send the data to all clients except the sender
+//     if (client !== sender && client.readyState === WebSocket.OPEN) {
+//       client.send(data)
+//     }
+//   })
+// }
