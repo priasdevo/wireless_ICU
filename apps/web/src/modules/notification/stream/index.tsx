c9b71fad25fd3bad5b87d83f1e7bfecb748bdeb3
CDGstudent <gearforcoding@gmail.com>
2023-10-03 10:44:52 +0700

    add a Typography

--- apps/web/src/modules/notification/stream/index.tsx
+++ apps/web/src/modules/notification/stream/index.tsx
@@ -1,3 +1,4 @@
+import { Typography } from '@mui/material'
 import { useRouter } from 'next/router'
 import React from 'react'
 
@@ -6,6 +7,7 @@ const VideoPlayer: React.FC = () => {
   const { id } = router.query
   return (
     <div style = {{display:'flex', flexDirection: 'column', alignItems:'center', marginTop:'10rem'}}>
+    <Typography variant = 'h5' color = '#ed7c31'>p1 p2</Typography>
     <video controls width="800">
       {id && (
         <source
